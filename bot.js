3247ac0b3445494e227eaf7d49988ff78496d07e
Heavyrisem <pyo1748@naver.com>
2019-09-01 07:55:33 +0900

    voice chjoin

---
+++ bot.js
@@ -0,0 +1,64 @@
+const Discord = require('discord.js');
+const config = require('./config');
+ 
+const client = new Discord.Client();
+ 
+
+var prefix = config.prefix;
+var voiceRoomName = 'None';
+var activity = '명령어 beta';
+
+client.on('ready', () => {
+  console.log(client.user.tag + ' 봇이 켜졌습니다. ');
+  client.user.setActivity(activity);
+})
+ 
+
+
+
+client.on('message', message => {
+  if(message.channel.type == 'dm') return;
+
+  if(message.content == '삐이이') {
+    message.channel.send('요오오오오옹');
+  }
+
+  //if(!message.content.startsWith(prefix)) return;
+
+
+ 
+  if(message.content.startsWith(prefix + '핑')) {
+    message.channel.send('현재 지박령 핑 상태에요 : ' + client.ping);
+  }
+
+  if(message.content.startsWith(prefix + 'join') || message.content.startsWith(prefix + '참가') || message.content.startsWith(prefix + 'ㅓㅐㅑㅜ')) {
+     if(message.member.voiceChannel && voiceRoomName == 'None' || !(message.member.voiceChannel == voiceRoomName)) {
+      //roomName = message.member.voiceChannel;
+      message.member.voiceChannel.join()
+        .then(connection => {
+          voiceRoomName = connection.channel.name;
+          message.channel.send(voiceRoomName + ' 에 연결했어요');
+          client.user.setActivity(voiceRoomName);
+        });
+
+      client.user.setActivity(voiceRoomName);
+    } else if(!(voiceRoomName == 'None')) {
+      message.channel.send('이미 ' + voiceRoomName + ' 에 연결되어 있어요');
+    } else {
+      message.channel.send('어디에 들어가야 할지 모르겠어요');
+    }
+  }
+
+  if(message.content.startsWith(prefix + 'leave') || message.content.startsWith(prefix + '나가')) {
+    voiceRoomName = 'None';
+    message.member.voiceChannel.leave();
+    message.channel.send('방에서 나갔어요');
+    client.user.setActivity(activity);
+  }
+
+  if(message.content.startsWith(prefix + '상태') || message.content.startsWith(prefix + 'status')) {
+    message.channel.send('지박령은 지금 ' + voiceRoomName + ' 에 연결되어 있고 핑 : '+ client.ping + 'ms, ' + activity + ' 플레이 중 입니다.');
+  }
+});
+ 
+client.login(config.token);
---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    token: 'NjE3MzEwMzY1OTE4ODIyNDIx.XWpYLw.6E27npAYasMqfR0qXy-I1UAiHAg',
+    prefix: '!'
+  }
