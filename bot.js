3b7f0d765cb1010cb1aa9b71f0a16c8bb2e1aa48
Heavyrisem <pyo1748@naver.com>
2019-09-01 13:59:01 +0900

    24h work

--- bot.js
+++ bot.js
@@ -17,13 +17,13 @@ client.on('ready', () => {
 
 
 client.on('message', message => {
-  //if(message.channel.type == 'dm') return;
+  if(message.channel.type == 'dm') return;
 
   if(message.content == '삐이이') {
     message.channel.send('요오오오오옹');
   }
 
-  //if(!message.content.startsWith(prefix)) return;
+  if(!message.content.startsWith(prefix)) return;
 
 
  
@@ -62,5 +62,5 @@ client.on('message', message => {
     message.channel.send('지박령은 지금 ' + voiceRoomName + ' 에 연결되어 있고 핑 : '+ client.ping + 'ms, ' + activity + ' 플레이 중 입니다.');
   }
 });
-console.log("토큰 : " + config.token);
-client.login("NjE3MzEwMzY1OTE4ODIyNDIx.XWtNlQ._UGjzi5E-Ko7K4VOCZAiByeT18w");
+console.log("토큰 : " + process.env.DISCORD_TOKEN);
+client.login(config.token);
