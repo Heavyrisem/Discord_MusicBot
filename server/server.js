ea8158bb66d499c6e758600a26ab86b22aed3ac7
Heavyrisem <pyo1748@naver.com>
2020-07-03 11:25:25 +0900

    핑 명령어 수정

--- server/server.js
+++ server/server.js
@@ -65,7 +65,8 @@ class server extends voicechannel {
         var e = this;
         if (e.ping != undefined) return; // ??
 
-        message.channel.send('``현재 핑은 ' + this.client.ping + 'ms 입니다.``'); // discord.js v12에서 Client.ws.ping으로 변경됨, 새로운 핑 시스템 추가 예정
+        const m = await message.channel.send("``핑 테스트``");
+        message.channel.send('``현재 핑은 ' + this.client.ping + 'ms, 레이턴시는 ' + m.createdTimestamp - message.createdTimestamp + 'ms 입니다.``'); // discord.js v12에서 Client.ws.ping으로 변경됨, 새로운 핑 시스템 추가 예정
     }
 
     updateMsg(message) {    // 서버 클래스에 메세지 넣는 기능, 마지막 메세지 저장용으로 변경해라
